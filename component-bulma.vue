0d4a81d9986be9bee98ec94e1d24e1ff1ea04a6d
Rainer Kuhn <ticktricktrack@gmail.com>
2016-12-06 23:23:29 +0000

    bulma css, components, transitions

---
+++ component-bulma.vue
@@ -0,0 +1,29 @@
+/*global Vue*/
+
+Vue.component("message", {
+  props: ["title", "body"],
+  data() {
+    return {
+      isVisible: true,
+    }
+  },
+
+  template: `
+    <transition name="fade">
+      <article class="message" v-show="isVisible">
+        <div class="message-header">
+          {{ title }}
+          <button type="button" @click="isVisible = false" class="button is-danger is-small is-pulled-right">
+            x
+          </button>
+        <div class="message-body">
+          {{ body }}
+        </div>
+      </article>
+    </transition
+  `
+})
+
+new Vue({
+  el: "#root",
+});
